93082ef4203dd569ad9daf410c504522bd298820
Eva Niehaus <eniehaus111@gmail.com>
2023-06-01 23:27:42 +0200

    add sort & search functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
+import PostFilter from './components/PostFilter';
+
 
 function App() {
     const [posts, setPosts] = useState([
-        { id: 1, title: 'JS', body: 'Description' },
-        { id: 2, title: 'JS-1', body: 'Description' },
-        { id: 3, title: 'JS-2', body: 'Description' }
+        { id: 1, title: 'bbb-1', body: 'Language' },
+        { id: 2, title: 'aaa-1', body: 'Library' },
+        { id: 3, title: 'Next-2', body: 'Description' }
     ]);
+    const [filter, setFilter] = useState({ sort: '', query: '' });
+
+    const sortedPosts = useMemo(() => {
+        console.log('Function worked');
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
+
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
+    };
+//Получаем post из дочернего компонента
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
     };
 
     return (
         <div className = 'App'>
             <PostForm create = { createPost } />
-            <PostList posts = { posts } title = 'List of JS Posts' />
+            <hr style = { { margin: ' 15px 0' } } />
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter} />
+            { sortedAndSearchedPosts.length
+                ?
+                <PostList remove = { removePost } posts = { sortedAndSearchedPosts } title = 'List of JS Posts' />
+                :
+                <h1
+                    style = { { textAlign: 'center' } }>No posts found
+                </h1>
+            }
         </div>
     );
 }
